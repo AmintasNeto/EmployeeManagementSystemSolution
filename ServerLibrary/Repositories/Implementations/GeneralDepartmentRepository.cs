190a4041a6006b0b23e0bb0c86ec4cf55a196b85
AmintasNeto <netorubok14@gmail.com>
2024-11-06 14:50:40 -0300

    Cria Interface generica e implementações dos modelos

---
+++ ServerLibrary/Repositories/Implementations/GeneralDepartmentRepository.cs
@@ -0,0 +1,52 @@
+﻿
+using BaseLibrary.Entities;
+using BaseLibrary.Responses;
+using Microsoft.EntityFrameworkCore;
+using ServerLibrary.Data;
+using ServerLibrary.Repositories.Contracts;
+
+namespace ServerLibrary.Repositories.Implementations
+{
+    public class GeneralDepartmentRepository(AppDbContext appDbContext) : IGenerictRepositoryInterface<GeneralDepartment>
+    {
+        public async Task<GeneralResponse> DeleteByID(int id)
+        {
+            var dep = await appDbContext.GeneralDepartments.FindAsync(id);
+            if (dep is null) return NotFound();
+
+            appDbContext.GeneralDepartments.Remove(dep);
+            await Commit();
+            return Success();
+        }
+
+        public async Task<List<GeneralDepartment>> GetAll() => await appDbContext.GeneralDepartments.ToListAsync();
+
+        public async Task<GeneralDepartment> GetById(int id) => await appDbContext.GeneralDepartments.FindAsync(id);
+
+        public async Task<GeneralResponse> Insert(GeneralDepartment item)
+        {
+            if (!await CheckName(item.Name!)) return new(false, "Department already created");
+            appDbContext.GeneralDepartments.Add(item);
+            await Commit();
+            return Success();
+        }
+
+        public async Task<GeneralResponse> Update(GeneralDepartment item)
+        {
+            var dep = await appDbContext.GeneralDepartments.FindAsync(item.Id);
+            if (dep is null) return NotFound();
+            dep.Name = item.Name;
+            await Commit();
+            return Success();
+        }
+
+        private static GeneralResponse NotFound() => new(false, "Sorry, department not foun");
+        private static GeneralResponse Success() => new(true, "Process completed");
+        private async Task Commit() => await appDbContext.SaveChangesAsync();
+        private async Task<bool> CheckName(string name)
+        {
+            var item = await appDbContext.GeneralDepartments.FirstOrDefaultAsync(x => x.Name!.ToLower().Equals(name.ToLower()));
+            return item is null;
+        }
+    }
+}

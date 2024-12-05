b95a4440408ba37507b99f25e94adf0567f2225f
AmintasNeto <netorubok14@gmail.com>
2024-12-05 17:59:14 -0300

    Implementa o frontend da funcionalidade de criar funcionario

--- BaseLibrary/Entities/Employee.cs
+++ BaseLibrary/Entities/Employee.cs
@@ -10,8 +10,6 @@ namespace BaseLibrary.Entities
         [Required]
         public string FileNumber { get; set; } = string.Empty;
         [Required]
-        public string Fullname { get; set; } = string.Empty;
-        [Required]
         public string JobName { get; set; } = string.Empty;
         [Required]
         public string Address { get; set; } = string.Empty;
